2457ae26ee6b05ddbe15aec44d01a493dff4a3f0
Zihad-1883 <your@email.com>
2026-03-10 06:18:45 +0600

    fix: add deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-   base: '/an-an-final/',
-  plugins: [react(),tailwindcss()],
+  base: '/an-an-final/',
+  plugins: [react(), tailwindcss()],
 })
